1e155eef72a4706c45b6fe405dd84a8acf352c8a
123Warper123 <slavik9071974rt@gmail.com>
2025-10-21 11:33:49 +0300

    kubs_history added

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
-int main() 
+int main()
 {
-  // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
-  cout << "$ ";
-  string input;
-  getline(std::cin, input);
-  cout << input << ": command not found" << endl;
-  
+    // Flush after every std::cout / std:cerr
+    cout << unitbuf;
+    cerr << unitbuf;
+    ofstream history_file("D:/VisualStudioProjects/kubsh/home/main_user/kubsh_history.txt", ios::app);
+    if (!history_file.is_open()) {
+        std::cerr << "Error: cannot open history file!" << std::endl;
+        return 1;
+    }
+    history_file << unitbuf; //for rebuffing
+
+    while (true) 
+    {
+        cout << "$ ";
+        string input;
+        if (!getline(cin, input) || (input == "\\q")) 
+        {//exit 1
+            // Ctrl+D
+            cout << "\nExit" << endl;
+            break;
+        }
+
+        history_file << input << "\n";
+    }
 }
